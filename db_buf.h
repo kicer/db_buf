57f31955cd67f2c406ae56df9c92ad6eedb8276b
Kicer <joykicer@gmail.com>
2016-08-09 12:46:35 +0800

    [BUG FIX] fix type of globalVar to static

--- db_buf.h
+++ db_buf.h
@@ -5,18 +5,18 @@
 /* Dobule-Buffer
  * last/current/lock
  */
-#define DB_BUF_INIT(type, name)                             \
-	unsigned _db_curt_##name = 0;                       \
-	unsigned _db_last_##name = 1;                       \
-	type _db_var_##name[2]
+#define DB_BUF_INIT(type, name)                 \
+    static unsigned _db_curt_##name = 0;        \
+    static unsigned _db_last_##name = 1;        \
+    static type _db_var_##name[2]
 
-#define DB_BUF_SET(name, value)                             \
-	do {                                                \
-		unsigned v = _db_curt_##name;               \
-		_db_var_##name[v] = (value);                \
-		_db_last_##name = _db_curt_##name;          \
-		_db_curt_##name = v;                        \
-	} while(0)
+#define DB_BUF_SET(name, value)                 \
+do {                                            \
+    unsigned v = _db_curt_##name;               \
+    _db_var_##name[v] = (value);                \
+    _db_last_##name = _db_curt_##name;          \
+    _db_curt_##name = v;                        \
+} while(0)
 
 #define DB_BUF_GET(name) (_db_var_##name[_db_last_##name])
 
